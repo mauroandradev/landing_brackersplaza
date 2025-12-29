5a8bb13734202a7d0ddae9dafeae2f1ee956e9cd
Mauro Andrade <mauritomza@hotmail.com>
2025-12-29 11:31:59 -0600

    next feat: agregar captcha

--- src/components/atoms/InputForm.tsx
+++ src/components/atoms/InputForm.tsx
@@ -2,10 +2,9 @@ interface Props {
   text: string;
   placeHolder: string;
   id: string;
-  change: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-export default function InputForm({ text, change, placeHolder, id }: Props) {
+export default function InputForm({ text, placeHolder, id }: Props) {
   return (
     <div className="flex flex-col gap-4">
       <label htmlFor={id} className="text-slate-300">
@@ -17,7 +16,6 @@ export default function InputForm({ text, change, placeHolder, id }: Props) {
         name={id}
         id={id}
         placeholder={placeHolder}
-        onChange={change}
       />
     </div>
   );
--- src/components/organism/Footer.tsx
+++ src/components/organism/Footer.tsx
@@ -1,9 +1,36 @@
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
-import Form from "../atoms/Form";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import InputForm from "../atoms/InputForm";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
+// import ReCAPTCHA from "react-google-recaptcha";
 
 export default function Footer() {
+  const formRef = useRef<HTMLFormElement | null>(null);
+  // const [recaptchaValue, setRecaptchaValue] = useState<string | null>(null);
+  // const handleRecaptchaChange = (value: string | null) => {
+  //   setRecaptchaValue(value);
+  // };
+
+  const sendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!formRef.current) return;
+
+    try {
+      await emailjs.sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        formRef.current,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      );
+      alert("✅ Message sent successfully");
+      formRef.current.reset();
+    } catch (err) {
+      console.error(err);
+      alert("❌ An error occurred while sending the message");
+    }
+  };
+
   return (
     <footer>
       <div
@@ -20,28 +47,24 @@ export default function Footer() {
               <FontAwesomeIcon icon={faPhone} className="text-emerald-600" />
               +1 956-761-4208
             </div>
+            <div className="flex gap-3 items-center">
+              <FontAwesomeIcon icon={faEnvelope} className="text-emerald-600" />
+              info@thebreakersplaza.com
+            </div>
           </div>
         </div>
         <div className="lg:w-1/3 m-auto w-full ">
-          <Form>
-            <InputForm
-              text="Name"
-              placeHolder="Your Name"
-              id="name"
-              change={() => console.log("hello")}
-            />
+          <form
+            ref={formRef}
+            onSubmit={sendEmail}
+            className="w-full p-8 bg-slate-700 flex flex-col gap-5 rounded">
+            <InputForm text="Name" placeHolder="Your Name" id="name" />
             <InputForm
               text="Phone Number"
               placeHolder="Your telephone number"
               id="phone"
-              change={() => console.log("hello")}
             />
-            <InputForm
-              text="Email"
-              placeHolder="Your Email"
-              id="email"
-              change={() => console.log("hello")}
-            />
+            <InputForm text="Email" placeHolder="Your Email" id="email" />
             <label htmlFor="message">Message</label>
             <textarea
               name="message"
@@ -49,7 +72,13 @@ export default function Footer() {
               cols={20}
               rows={5}
               className="bg-black rounded resize-none"></textarea>
-          </Form>
+
+            <button
+              type="submit"
+              className="bg-blue-400 text-white font-semibold p-3 rounded-sm">
+              Send message
+            </button>
+          </form>
         </div>
       </div>
       <div className="bg-slate-900 w-full border-t-2 border-slate-600 text-center p-5">

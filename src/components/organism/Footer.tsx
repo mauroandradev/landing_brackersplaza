aefa185c81bc23f0a5efe515a5da583fc120cbe6
Mauro Andrade <mauritomza@hotmail.com>
2026-05-04 09:30:11 -0500

    chore:se elimino contacto via mail

--- src/components/organism/Footer.tsx
+++ src/components/organism/Footer.tsx
@@ -49,60 +49,17 @@ export default function Footer() {
     <footer>
       <div className="w-full flex flex-wrap bg-slate-900 min-h-[40dvh] gap-10 p-10">
         <div className="lg:w-1/3 m-auto flex flex-col gap-10 text-xl">
-          <h2 className="font-bold text-5xl">Contact Us</h2>
+          <h2 className="font-bold text-5xl lg:text-center">Contact Us</h2>
           <p>
             Do you have questions? We're here to help you plan your perfect
             stay. Write to us or call us for more information.
           </p>
           <div className="flex flex-col gap-5">
-            <div className="flex gap-3 items-center">
+            <div className="flex gap-3 items-center lg:justify-center">
               <FontAwesomeIcon icon={faPhone} className="text-emerald-600" />
               +1 956-761-4208
             </div>
-            <div className="flex gap-3 items-center">
-              <FontAwesomeIcon icon={faEnvelope} className="text-emerald-600" />
-              info@thebreakersplaza.com
-            </div>
           </div>
-        </div>
-
-        <div className="lg:w-1/3 m-auto w-full">
-          <form
-            ref={formRef}
-            onSubmit={handleSubmit}
-            className="w-full p-8 bg-slate-700 flex flex-col gap-5 rounded">
-            <InputForm text="Name" placeHolder="Your Name" id="name" />
-            <InputForm
-              text="Phone Number"
-              placeHolder="Your telephone number"
-              id="phone"
-            />
-            <InputForm text="Email" placeHolder="Your Email" id="email" />
-
-            <label htmlFor="message">Message</label>
-            <textarea
-              name="message"
-              id="message"
-              cols={20}
-              rows={5}
-              className="bg-black rounded resize-none"
-            />
-
-            {/* reCAPTCHA v2 INVISIBLE */}
-            <ReCAPTCHA
-              ref={recaptchaRef}
-              sitekey={captchaKey}
-              size="invisible"
-              onChange={handleCaptchaSuccess}
-            />
-
-            <button
-              type="submit"
-              disabled={sending}
-              className="bg-blue-400 text-white font-semibold p-3 rounded-sm disabled:opacity-60">
-              {sending ? "Sending..." : "Send message"}
-            </button>
-          </form>
         </div>
       </div>
 
